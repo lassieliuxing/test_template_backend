102ab2b3cda4cafe0f3207f76e2585d1aa067bdc
刘星 <753880957@qq.com>
2024-06-24 16:26:12 +0800

    fix picture

--- backend/test_file.py
+++ backend/test_file.py
@@ -3,6 +3,7 @@ import requests
 def test_file():
     # 模拟发送图片请求
     url="http://127.0.0.1:5000/file"
-    file={'file':open("C:/Users/打工人/Desktop/测试资料/图片/logo.jpg",'rb')}
+    # file={'file':open("C:/Users/打工人/Desktop/测试资料/图片/logo.jpg",'rb')}
+    file={'file':open("./recource/test.jpg",'rb')}
     r=requests.post(url,files=file)
     assert r.status_code==200
